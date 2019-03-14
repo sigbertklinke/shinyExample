9046e553d23a640508f505423719a0b281f08ee4
Sigbert Klinke <sigbert@wiwi.hu-berlin.de>
2019-03-14 11:50:35 +0100

    clean inst/examples/www,run,rds

--- inst/examples/run/noprg_R00480004800000000000000.R
+++
@@ -1,8 +0,0 @@
-dev.off <- pdf <- postscript <- png <- bmp <- jpeg <- tiff <- function(...) {invisible(NULL)}
-source <- function(file, ...) { file <- list.files(path="..", pattern=file, recursive=TRUE, full.names=TRUE)[1]; cat(file, "
-"); base::source(file=file, ...)}
-grDevices::pdf("noprg_R00480004800000000000000.pdf", width=7.0, height=7.0)
-cat("<!--START-->\n")
-"Please select a file"
-#<!--END-->
-grDevices::dev.off()
